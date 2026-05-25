d799fa3cf75d928788ba7123e6da63590de4a7f3
Md. Seam <seam5559@gmail.com>
2026-05-25 18:37:19 +0600

    Create Project

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿class Assignment2
+{
+    public static void Main(string[] args)
+    {
+        string name = "MacBook Pro";
+        int price = 100000;
+        string category = "Laptop";
+        bool isAvailable = true;
+        int sold = 5;
+        Console.WriteLine($"Name: {name}");
+        Console.WriteLine($"Price: {price}");
+        Console.WriteLine($"Category: {category}");
+        Console.WriteLine($"Availability: {isAvailable}");
+        Console.WriteLine($"Sold: {sold}");
+    }
+}
